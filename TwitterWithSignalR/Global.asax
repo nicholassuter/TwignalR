8c87e3505f63064808a52d45ec71d5f7a1b50026
nicholas.suter@cellenza.com <nicholas.suter@cellenza.com>
2014-02-09 18:34:53 +0100

    Project init

---
+++ TwitterWithSignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwitterWithSignalR.MvcApplication" Language="C#" %>
---
+++ TwitterWithSignalR/Hubs/TwignalRHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SignalRChat
+{
+    public class TwignalRHub : Hub
+    {
+        static int nbMessages;
+        public void SendToHub(string name, string message)
+        {
+            var filteredMessage = message.Replace(',', ' ').Replace('.', ' ').Replace('!', ' ').Replace('?', ' ');
+            var destinataires = filteredMessage.Split(' ').Where(w => w.StartsWith("@")).ToArray();
+
+            nbMessages++;
+            Clients.All.sendToClients(name, message, nbMessages, destinataires);
+        }
+    }
+}
