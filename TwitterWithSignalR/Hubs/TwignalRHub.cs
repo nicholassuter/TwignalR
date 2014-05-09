3eff5c4d12d51a81b94f5abcb9a110e0303962e2
nicholas.suter@cellenza.com <nicholas.suter@cellenza.com>
2014-05-09 17:50:28 +0200

    Des DM !

--- TwitterWithSignalR/Hubs/TwignalRHub.cs
+++ TwitterWithSignalR/Hubs/TwignalRHub.cs
@@ -1,19 +1,91 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System.Threading.Tasks;
+using System.Web.UI;
+using Microsoft.AspNet.SignalR;
 using System.Collections.Generic;
 using System.Linq;
+using TwitterWithSignalR.Models;
 
 namespace SignalRChat
 {
     public class TwignalRHub : Hub
     {
-        static int nbMessages;
+        static int _nbMessages;
+
+        private readonly static ConnectionMapping<string> Connections = new ConnectionMapping<string>();
+
         public void SendToHub(string name, string message)
         {
-            var filteredMessage = message.Replace(',', ' ').Replace('.', ' ').Replace('!', ' ').Replace('?', ' ');
-            var destinataires = filteredMessage.Split(' ').Where(w => w.StartsWith("@")).ToArray();
+            _nbMessages++;
 
-            nbMessages++;
-            Clients.All.sendToClients(name, message, nbMessages, destinataires);
+            if (message.StartsWith("d "))
+            {
+                SendDm(name, message, _nbMessages);
+            }
+            else
+            {
+                var filteredMessage = message.Replace(',', ' ').Replace('.', ' ').Replace('!', ' ').Replace('?', ' ');
+                var destinataires = filteredMessage.Split(' ').Where(w => w.StartsWith("@")).ToArray();
+
+                Clients.All.sendToClients(name, message, _nbMessages, destinataires);
+            }
+            
+        }
+
+        private void SendDm(string from, string message, int messageId)
+        {
+            string[] words = message.Split(' ');
+            if (words.Count() > 2)
+            {
+                string sendTo = words[1];
+                string connectionId = Connections.GetConnection(sendTo);
+                if (!string.IsNullOrEmpty(connectionId))
+                {
+                    string messageToSend = string.Join(" ", words.Skip(2));
+                    Clients.Client(connectionId).sendDm(from, messageToSend, messageId);
+                    if (connectionId != Context.ConnectionId)
+                    {
+                        Clients.Caller.sendDm(from, messageToSend, messageId);                        
+                    }
+                }
+                else
+                {
+                    Clients.Caller.systemNotification(sendTo + " n'est pas connecté.", "#FF0000");
+                }
+            }
+        }
+
+        public void NotifyConnection(string userName)
+        {
+            Connections.Add(userName, Context.ConnectionId);
+            Clients.All.systemNotification(userName + " vient de se connecter.", "#81F781");
+        }
+
+        //public override Task OnConnected()
+        //{
+        //    string from = Context.User.Identity.Name;
+
+        //    _connections.Add(from, Context.ConnectionId);
+
+        //    return base.OnConnected();
+        //}
+
+        public override Task OnDisconnected()
+        {
+            Connections.Remove(Context.ConnectionId);
+
+            return base.OnDisconnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            if (!Connections.GetConnection(name).Contains(Context.ConnectionId))
+            {
+                Connections.Add(name, Context.ConnectionId);
+            }
+
+            return base.OnReconnected();
         }
     }
 }
---
+++ TwitterWithSignalR/Models/ConnectionMapping.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterWithSignalR.Models
+{
+    public class ConnectionMapping<T>
+    {
+        private readonly Dictionary<T, string> _connections =
+            new Dictionary<T, string>();
+
+        public int Count
+        {
+            get
+            {
+                return _connections.Count;
+            }
+        }
+
+        public void Add(T key, string connectionId)
+        {
+            lock (_connections)
+            {
+                if (!_connections.ContainsKey(key))
+                {
+                    _connections.Add(key, connectionId);
+                }
+            }
+        }
+
+        public string GetConnection(T key)
+        {
+            if (_connections.ContainsKey(key))
+            {
+                return _connections[key];
+            }
+
+            return null;
+        }
+
+        public void Remove(string connectionId)
+        {
+            lock (_connections)
+            {
+                if (_connections.ContainsValue(connectionId))
+                {
+                    T key = _connections.FirstOrDefault(x => x.Value == connectionId).Key;
+                    _connections.Remove(key);
+                }
+            }
+        }
+    }
+}
